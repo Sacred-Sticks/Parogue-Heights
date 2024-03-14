38018e95c48505bb055db7045f9c34c11d89dda5
Lucas Ackman <AckmanLD@gmail.com>
2024-03-14 00:20:50 -0500

    Added Prototype Tower Generator

--- The Endless Ascent/Assets/Kickstarter/Attributes/Editor/EnumDataDrawer.cs
+++ The Endless Ascent/Assets/Kickstarter/Attributes/Editor/EnumDataDrawer.cs
@@ -1,31 +1,43 @@
-using System;
 using UnityEditor;
 using UnityEngine;
 
 [CustomPropertyDrawer(typeof(EnumData))]
 public class EnumDataDrawer : PropertyDrawer
 {
-    private SerializedProperty array;
-    
+    private SerializedProperty _array;
+
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        string path = property.propertyPath;
-        if (array == null)
+        EnumData enumData = attribute as EnumData;
+        string propertyPath = property.propertyPath;
+
+        if (_array == null)
         {
-            array = property.serializedObject.FindProperty(path[..path.LastIndexOf('.')]);
-            if (array == null)
+            _array = property.serializedObject.FindProperty(propertyPath.Substring(0, propertyPath.IndexOf(".")));
+            if (_array == null)
             {
-                EditorGUI.LabelField(position, "Use EnumData Attribute on Arrays");
+                Debug.LogError("EnumData attribute must be used on an array field");
                 return;
             }
         }
-        
-        var enumData = (EnumData)attribute;
-        if (array.arraySize != enumData.Names.Length)
-            array.arraySize = enumData.Names.Length;
 
-        int index = Convert.ToInt32(path[(path.IndexOf('[') + 1)..].Replace("]", ""));
+        if (_array.arraySize > enumData.Names.Length)
+            _array.arraySize = enumData.Names.Length;
+
+        int startIndex = propertyPath.IndexOf("[") + 1;
+        int endIndex = propertyPath.IndexOf("]");
+
+        string indexStr = propertyPath.Substring(startIndex, endIndex - startIndex);
+
+        int index = int.Parse(indexStr);
+
         label.text = enumData.Names[index];
+
         EditorGUI.PropertyField(position, property, label, true);
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
     }
 }
--- The Endless Ascent/Assets/_Project/_Scripts/Proc. Gen/TowerGenerator.cs
+++ The Endless Ascent/Assets/_Project/_Scripts/Proc. Gen/TowerGenerator.cs
@@ -1,51 +1,91 @@
+using Kickstarter.DependencyInjection;
+using Kickstarter.Extensions;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace The_Endless_Ascent
 {
-    public class TowerGenerator : MonoBehaviour, IGenerator
+    public class TowerGenerator : MonoBehaviour, IGenerator, IDependencyProvider
     {
-        [SerializeField, /*EnumData(typeof(IGenerator.LayerHeight)),*/] private Layer[] layers;
+        [Provide] private IGenerator generator => this;
+
+        [SerializeField, EnumData(typeof(IGenerator.LayerHeight)),] private Layer[] layers;
 
         private int layersGenerated;
-        private IGenerator.LayerHeight currentLayerCategory;
+        private IGenerator.LayerHeight currentLayerCategory = IGenerator.LayerHeight.Initial;
+        private Vector3 offset;
 
-        // Constants
+        // Constants and Readonly
         private const int initialLayers = 5;
+        private readonly Dictionary<IGenerator.LayerHeight, Layer> sortedPrefabs 
+            = new Dictionary<IGenerator.LayerHeight, Layer>();
 
-        #region Generator
-        public void GenerateInitialLayers()
+        #region UnityEvents
+        private void Awake()
         {
-            for (int i = 0; i < initialLayers; i++)
-            {
-                GenerateLayer();
-            }
+            sortedPrefabs.LoadDictionary(layers);
         }
 
-        public void GenerateLayer()
+        private void Start()
         {
-
+            GenerateInitialLayers();
         }
         #endregion
 
-        [System.Serializable]
-        private struct Layer
+        #region Generator
+        public void GenerateLayer()
         {
-            public GameObject[] layerVariants;
+            var layer = sortedPrefabs[currentLayerCategory];
+            var selectedVariant = layer.LayerVariants[Random.Range(0, layer.LayerVariants.Length)];
+            Instantiate(selectedVariant, transform.position + offset, Quaternion.identity, transform);
+            offset += Vector3.up * layer.LayerHeight;
+            IncrementLayer();
+        }
+        #endregion
+
+        private void GenerateInitialLayers()
+        {
+            for (int i = 0; i < initialLayers; i++)
+                GenerateLayer();
+        }
+
+        private void IncrementLayer()
+        {
+            layersGenerated++;
+            if (layersGenerated >= (int) currentLayerCategory)
+            {
+                currentLayerCategory = currentLayerCategory switch
+                {
+                    IGenerator.LayerHeight.Initial => IGenerator.LayerHeight.Easy,
+                    IGenerator.LayerHeight.Easy => IGenerator.LayerHeight.Medium,
+                    IGenerator.LayerHeight.Medium => IGenerator.LayerHeight.Hard,
+                    IGenerator.LayerHeight.Hard => IGenerator.LayerHeight.Impossible,
+                    _ => currentLayerCategory
+                };
+            }
+        }
+
+        [System.Serializable] private struct Layer
+        {
+            [SerializeField] private GameObject[] _layerVariants;
+            [SerializeField] private float _layerHeight;
+
+            public GameObject[] LayerVariants => _layerVariants;
+            public float LayerHeight => _layerHeight;
         }
     }
 
     public interface IGenerator
     {
-        public void GenerateInitialLayers();
         public void GenerateLayer();
 
         public enum LayerHeight
         {
-            Initial = 0,
-            Easy = 10,
-            Medium = 30,
-            Hard = 70,
-            Impossible = 150,
+            Initial = 1,
+            Easy = 9,
+            Medium = 33,
+            Hard = 65,
+            Impossible,
         }
     }
 }
