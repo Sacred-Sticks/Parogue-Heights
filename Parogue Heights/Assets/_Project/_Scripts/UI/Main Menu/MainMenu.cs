92804e089cfbf902295e328bb08115956b4388de
Lucas Ackman <AckmanLD@gmail.com>
2024-03-27 22:13:09 -0500

    Made main menu more pleasant

--- Parogue Heights/Assets/_Project/_Scripts/UI/Main Menu/MainMenu.cs
+++ Parogue Heights/Assets/_Project/_Scripts/UI/Main Menu/MainMenu.cs
@@ -12,30 +12,33 @@ namespace Parogue_Heights
 
         [SerializeField] private StyleSheet stylesheet;
 
-        private string gameplaySceneGroupStr = "Gameplay";
-        private const string rootClassStr = "root";
-        private const string playButtonClassStr = "play_button";
+        private string gameplayStr = "Gameplay";
+        private const string rootStr = "root";
+        private const string titleStr = "title";
+        private const string playButtonStr = "play_button";
 
         #region UnityEvents
         private void Awake()
         {
             var root = GetComponent<UIDocument>().rootVisualElement;
             root.styleSheets.Add(stylesheet);
-            root.AddToClassList(rootClassStr);
+            root.AddToClassList(rootStr);
             BuildMenu(root);
         }
         #endregion
 
         private void BuildMenu(VisualElement root)
         {
-            var button = root.CreateChild<Button>(playButtonClassStr);
+            var title = root.CreateChild<Label>(titleStr);
+            title.text = "Parogue Heights";
+            var button = root.CreateChild<Button>(playButtonStr);
             button.text = "Play";
             button.clickable.clicked += StartGame;
         }
 
         private void StartGame()
         {
-            sceneLoader.LoadSceneGroup(gameplaySceneGroupStr);
+            sceneLoader.LoadSceneGroup(gameplayStr);
         }
     }
 }
