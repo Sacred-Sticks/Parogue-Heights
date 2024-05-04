5809f14d8ac05dbd885e8b10fc32f4d1478bf9e8
Lucas Ackman <AckmanLD@gmail.com>
2024-05-03 19:40:05 -0500

    Squashed commit of the following:
    commit 1c6e491be14e92d64a387f7095a6df25ae13528d
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Fri May 3 19:39:51 2024 -0500
    
        Set camera aiming offset X to 0.5
    
    commit 273bddd4ace3abc37e1a4617531b54f58651de30
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Fri May 3 19:35:43 2024 -0500
    
        Moved appropriate functionality to the camera mediator
    
    commit 3746b518fd552617506ebc01d6e0a715445164a3
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Fri May 3 13:42:39 2024 -0500
    
        Set Pause Input to use an explicit method function
    
    commit a2796d875b57239da357b7825e88a6e5fd3b76b4
    Merge: 8b7b48f 8cea853
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Fri May 3 13:31:19 2024 -0500
    
        Merge branch 'main' into Character-Controller
    
    commit 8b7b48f96ea09b55412c0babb36b75fd5f863bdb
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Fri May 3 01:42:47 2024 -0500
    
        Decreased Aim Damping on Virtual Camera
    
    commit a642f4953884019373c430aceed0402422e392c9
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Fri May 3 01:28:44 2024 -0500
    
        Increased vertical range of camera from 60 to 80
    
    commit 1f6e7f1e4fe30ef1e68bf78b77e75bdcd23538be
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Fri May 3 01:26:45 2024 -0500
    
        Cut rotation sensitivity in half
    
    commit 69004a131da72a5d50b744c4edba17a12357250b
    Merge: 3121a3e 804ae39
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Fri May 3 01:24:09 2024 -0500
    
        Merge branch 'main' into Character-Controller
    
    commit 3121a3e7d8736cf3bda28790150aed579def52a8
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Mon Apr 29 22:02:07 2024 -0500
    
        Increased Vertical drag on camera
    
    commit e237cbef80cb1d54858d2f315ea992d4b7cb9c7b
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Mon Apr 29 21:57:22 2024 -0500
    
        Added Camera Shake when moving quickly
    
    commit c573660d71d884b40ccfb1b15dc00dae6c4082ff
    Merge: c18fcde 3f51d9e
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Mon Apr 29 21:05:06 2024 -0500
    
        Merge branch 'main' into Character-Controller
    
    commit c18fcdefdaff7a9df12264b5b76506bdc8b89629
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Sun Apr 28 01:36:37 2024 -0500
    
        Adjusted Camera Settings
    
    commit b4e558cb54513d4f99a0bffca52505bc92261755
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Sun Apr 28 01:07:41 2024 -0500
    
        Removed Unused GameObjects in Rotation Controller
    
    commit 576fbd2c87ea0f6efa0eaf1887f2136b6bd8dacf
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Sun Apr 28 00:30:09 2024 -0500
    
        Added multiple Camera Rigs to Rotation Controller
    
    commit fc79c062e7eed4b18c78d84bc9d7dda390bf997e
    Merge: 4094bf9 7ccc246
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Sat Apr 27 21:07:49 2024 -0500
    
        Merge branch 'main' into Character-Controller
    
    commit 4094bf9124d9f86f519407c14a20164ed615e174
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu Apr 18 23:26:09 2024 -0500
    
        Added Ground Rush Functionality
    
    commit 707f679bc78ee151b335dfe86efff494c95c4dc8
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu Apr 18 23:06:00 2024 -0500
    
        Added Sprinting Speed Control
    
    commit 7d457ede271aa2095b4b913b73b997e1bc788f77
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu Apr 18 22:42:30 2024 -0500
    
        Adjusted Camera to rotate freely around character model
    
    commit 04dc1648c71ce58dac3da036afd2dc5922817c6e
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu Apr 18 21:00:09 2024 -0500
    
        Removed unused legacy variables
    
    commit ea7e6ef2dfaad19e2e64d72b741798060161a13b
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu Apr 18 20:28:06 2024 -0500
    
        Added more air control to character controller
    
    commit 452580d518c986d36145683dd3b44f03e09e41b8
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu Apr 18 18:20:39 2024 -0500
    
        Added One Way Platforms

--- Parogue Heights/Assets/_Project/_Scripts/Controllers/CameraMediator.cs
+++ Parogue Heights/Assets/_Project/_Scripts/Controllers/CameraMediator.cs
@@ -6,20 +6,28 @@ namespace Parogue_Heights
 {
     public class CameraMediator : MonoBehaviour, IDependencyProvider
     {
-        [Provide] private CinemachineVirtualCamera virtualCamera;
-        [Provide] private Cinemachine3rdPersonFollow follow;
-        private CinemachineBasicMultiChannelPerlin noise;
+        [Provide] private CameraMediator Mediator() => this;
 
         [SerializeField] private AnimationCurve cameraShakeRate;
         [SerializeField] private float cameraShakeMultiplier;
         [SerializeField] private float cameraShakeHighSpeed;
 
         private Rigidbody body;
+        private CinemachineVirtualCamera virtualCamera;
+        private Cinemachine3rdPersonFollow follow;
+        private CinemachineBasicMultiChannelPerlin noise;
+
+        private RigSettings highRig;
+        private RigSettings midRig;
+        private RigSettings lowRig;
 
         #region UnityEvents
         private void Awake()
         {
             virtualCamera = GetComponent<CinemachineVirtualCamera>();
+            highRig = new RigSettings(4.0f, virtualCamera.LookAt.transform.localPosition + Vector3.up * 0.25f);
+            midRig = new RigSettings(6.0f, virtualCamera.LookAt.transform.localPosition);
+            lowRig = new RigSettings(3.0f, virtualCamera.LookAt.transform.localPosition - Vector3.up * 0.5f);
 
             noise = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
 
@@ -41,6 +49,34 @@ namespace Parogue_Heights
         }
         #endregion
 
+        public void CalibrateByXAngle(float angle, float verticalRange)
+        {
+            if (angle > 180)
+                angle = angle - 360;
+            float lowValue = midRig.CameraDistance;
+            float highValue = lowRig.CameraDistance;
+            if (angle > 0)
+            {
+                lowValue = midRig.CameraDistance;
+                highValue = highRig.CameraDistance;
+            }
 
+            float range = Mathf.Lerp(lowValue, highValue, Mathf.Abs(angle / verticalRange));
+            follow.CameraDistance = range;
+
+            virtualCamera.LookAt.transform.localPosition = Vector3.Lerp(lowRig.Position, highRig.Position, Mathf.Abs(angle + verticalRange) / (verticalRange * 2));
+        }
+
+        private struct RigSettings
+        {
+            public RigSettings(float cameraDistance, Vector3 position)
+            {
+                CameraDistance = cameraDistance;
+                Position = position;
+            }
+
+            public float CameraDistance { get; }
+            public Vector3 Position { get; }
+        }
     }
 }
