103ad26aecac0f260af08e0271de756e46145da2
Lucas Ackman <AckmanLD@gmail.com>
2024-05-05 00:52:15 -0500

    Added Bugged Pause Menu
    Button hitboxes are in the wrong place for some reason, even though the same menus are in the main menu.

--- Parogue Heights/Assets/_Project/_Scripts/UI/Menus/Menu.cs
+++ Parogue Heights/Assets/_Project/_Scripts/UI/Menus/Menu.cs
@@ -3,20 +3,31 @@ using UnityEngine.UIElements;
 
 namespace Parogue_Heights
 {
+    [RequireComponent(typeof(UIDocument))]
     public abstract class Menu : MonoBehaviour
     {
         [SerializeField] protected StyleSheet styleSheet;
 
         protected VisualElement root;
 
+        private bool isOpen;
+
         public virtual void Open()
         {
             root.style.display = DisplayStyle.Flex;
+            isOpen = true;
         }
 
         public virtual void Close()
         {
             root.style.display = DisplayStyle.None;
+            isOpen = false;
+        }
+
+        public void ToggleMenu()
+        {
+            System.Action action = isOpen ? Close : Open;
+            action();
         }
 
         private void OnValidate()
