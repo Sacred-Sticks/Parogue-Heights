234cc36f2f22666d38275837813d026dcfc9c1b9
Lucas Ackman <AckmanLD@gmail.com>
2024-05-06 00:00:21 -0500

    Squashed commit of the following:
    commit ac86ba81d4074cbe6ccc276650af867f97bea161
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Sun May 5 23:59:35 2024 -0500
    
        Added Personal Credits to Main Menu
    
    commit 6e9f340bba0063020eebe3e80094b0412c58c93d
    Merge: db6e90b 606370f
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Sun May 5 23:29:51 2024 -0500
    
        Merge branch 'main' into UI
    
    commit db6e90b97f6528a99b2e09a138a56089ae58e04d
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Sun May 5 23:13:54 2024 -0500
    
        Fixed Options Menu close button styling to match main menu
    
    commit cd60c2a66ac2f95d12530213de7ae86d3c2ea322
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Sun May 5 22:54:57 2024 -0500
    
        Fixed bad button hitboxes in Pause Menu
    
    commit 103ad26aecac0f260af08e0271de756e46145da2
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Sun May 5 00:52:15 2024 -0500
    
        Added Bugged Pause Menu
    
        Button hitboxes are in the wrong place for some reason, even though the same menus are in the main menu.
    
    commit 00e971ac926c1e9c507ecaf0aa182032b9681104
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Sat May 4 22:49:02 2024 -0500
    
        Renamed Folder
    
    commit 3a6d471d7ef9017a14ea62c630bec544bbd97a33
    Merge: 49a5d39 5809f14
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Fri May 3 21:09:27 2024 -0500
    
        Merge branch 'main' into UI
    
    commit 49a5d391ea97c8f824937a61f482d5e54d1eff8a
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Fri May 3 01:02:52 2024 -0500
    
        Renamed Rebound Spell to Dashback
    
    commit b3e8900227b8678c74b088dbe7692c1013b0af85
    Merge: 7ffc4ac d55ca9d
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Fri May 3 00:54:05 2024 -0500
    
        Merge branch 'main' into UI
    
    commit 7ffc4ac068cd8ba3a23adf46a1309b7265761f21
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu May 2 22:17:17 2024 -0500
    
        Made the active inventory slot more noticeable
    
    commit 000933e13471d36cb16453c363b9770b233ae275
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu May 2 21:15:47 2024 -0500
    
        Opened Main Menu before closing sub-menus
    
    commit 3f16846c1654b1338fa14935df41308d5585d624
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu May 2 20:56:03 2024 -0500
    
        Modified Menu Close functionality
    
    commit 9e69fe76411f902a9daded571960db6cfb1e9854
    Merge: a0b41e9 a6a2a93
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu May 2 19:30:24 2024 -0500
    
        Merge branch 'main' into UI
    
    commit a0b41e9b9bfc4fbede61996053bf29d8fb9d8854
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu May 2 00:32:04 2024 -0500
    
        Raised Points per Layer
    
    commit 80208acf86f54874195b7119462e799c9d9bc590
    Author: Lucas Ackman <AckmanLD@gmail.com>
    Date:   Thu May 2 00:15:41 2024 -0500
    
        Implemented Height Scores

---
+++ Parogue Heights/Assets/_Project/_Scripts/UI/Menus/Credits.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.UIElements;
+
+namespace Parogue_Heights
+{
+    public class Credits : Menu
+    {
+        private const string downwardStr = "downward";
+        private const string creditsStr = "credits";
+
+        #region UnityEvents
+        private void Start()
+        {
+            root = GetComponent<UIDocument>().rootVisualElement;
+            BuildDocument();
+        }
+        #endregion
+
+        protected override void BuildDocument()
+        {
+            if (root == null) 
+                return;
+            root.styleSheets.Add(styleSheet);
+
+            var downwardContainer = root.CreateChild<VisualElement>(downwardStr);
+
+            var credits = downwardContainer.CreateChild<Label>(creditsStr);
+            credits.text = "A Game by Lucas Ackman";
+        }
+    }
+}
